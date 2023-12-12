5ad8edb0fcd8e5743de6d6e7c02a51921c20e61e
Kevoelie <Kevoelie+github@gmail.com>
2023-12-12 20:25:11 +0100

    Update banner.php

--- cmd/banner.php
+++ cmd/banner.php
@@ -6,13 +6,6 @@ class banner_cmd extends base_cmd
     }
     function run() : string {
         $banner_play = new Playscii("AoC_files/Banner.psci");
-        //$banner = file_get_contents("AoC_files/banner.ans");
-        //$banner = str_replace("ESC", "\x1b", $banner);
-        //$d="";
-        //for ($i = 0; $i < 255; $i++){
-        //    $d.= iconv("cp437", 'utf-8', chr($i));
-        //}
-        //file_put_contents("AoC_files/Playscii_Charsets/dos_loopmap_d.char",$d);
         return json_encode(array("data" => $banner_play, "run" => "loopLines"), 128);
     }
 }
