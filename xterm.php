9942b337092eb5277678b90ba95935305b03c06f
Kevoelie <Kevoelie+github@gmail.com>
2023-12-12 20:56:16 +0100

    Update xterm.php

--- xterm.php
+++ xterm.php
@@ -1,7 +1,9 @@
 <?php
     require_once 'vendor/autoload.php';
     use CHH\Optparse;
+    session_start();
     header("Content-Type: text/javascript");
+    define("file_path","AoC_files/");
     spl_autoload_register(function ($class_name) {
         if (str_contains($class_name,"_cmd")) {
             include "cmd/".str_replace("_cmd","",$class_name) . '.php';
@@ -13,10 +15,14 @@
     });
     $response =array("data"=>"","run"=> "addLine");
     $cmd = (urlencode($_GET["cmd"])=== "%F0%9F%8E%85") ? "banner" : html_entity_decode($_GET["cmd"], ENT_SUBSTITUTE) ;
-
+    $argv = (!isset($_GET["argv"])) ? "" : html_entity_decode(base64_decode($_GET["argv"]), ENT_SUBSTITUTE);
     $cmd_list=array(
         "banner"=>new banner_cmd,
         "help"=> "You obviously already know what this does"
+        "ls"=>new ls_cmd,
+        "sl"=>new sl_cmd,
+        "cowsay"=>new cowsay
+        //"pwd"=>new pwd_cmd()
     );
     if (strtolower($cmd)==="br") {
         $response["run"] = "br";
@@ -31,7 +37,7 @@
     }elseif ($cmd==="man") {
         $response["data"] = htmlentities($cmd) . ": command not found.";
     }elseif (isset($cmd_list[$cmd])) {
-        $data= json_decode($cmd_list[$cmd]->run(),true);
+        $data= json_decode($cmd_list[$cmd]->run($argv),true);
         $response["run"] = $data["run"];
         $response["data"] = $data["data"];
     }else {
