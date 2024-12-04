fe24a02832e72c4e9e712c557f7037c65e6d439a
Kevoelie <Kevoelie+github@gmail.com>
2024-12-04 21:24:06 +0100

    Update banner.php

--- cmd/banner.php
+++ cmd/banner.php
@@ -4,7 +4,7 @@ class banner_cmd extends base_cmd
     public function __construct() {
         $this->help = "Display the header";
     }
-    function run() : string {
+    function serve() : string {
         $banner_play = new Playscii(file_path."Banner.psci");
         return json_encode(array("data" => $banner_play, "run" => "loopLines"), 128);
     }
