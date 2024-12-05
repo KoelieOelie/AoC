0cc7b501257e96afffd69b6b595dde4e76cd2f94
KoelieOelie <kevoelie+github@gmail.com>
2024-12-05 13:30:45 +0100

    modified:   cmd/os.php

--- cmd/os.php
+++ cmd/os.php
@@ -86,13 +86,75 @@ class os_cmd extends base_cmd
                 $this->params=false;
                 break;
             case 'ls':
+                /**
+                 * Usage: ls [-1AaCxdLHRFplinshrSXvctu] [-w WIDTH] [FILE]...
+                 *
+                 * List directory contents
+                 *  
+                 *         -1      One column output
+                 *         -a      Include entries which start with .
+                 *         -A      Like -a, but exclude . and ..
+                 *         -x      List by lines
+                 *         -d      List directory entries instead of contents
+                 *         -L      Follow symlinks
+                 *         -H      Follow symlinks on command line
+                 *         -R      Recurse
+                 *         -p      Append / to dir entries
+                 *         -F      Append indicator (one of * /=@|) to entries
+                 *         -l      Long listing format
+                 *         -i      List inode numbers
+                 *         -n      List numeric UIDs and GIDs instead of names
+                 *         -s      List allocated blocks
+                 *         -lc     List ctime
+                 *         -lu     List atime
+                 *         --full-time     List full date and time
+                 *         -h      Human readable sizes (1K 243M 2G)
+                 *         --group-directories-first
+                 *         -S      Sort by size
+                 *         -X      Sort by extension
+                 *         -v      Sort by version
+                 *         -t      Sort by mtime
+                 *         -tc     Sort by ctime
+                 *         -tu     Sort by atime
+                 *         -r      Reverse sort order
+                 *         -w N    Format N columns wide
+                 *         --color[={always,never,auto}]   Control coloring
+                 * 
+                 */
                 $this->init_cli();
                 $this->params = true;
                 $this->cli->addRule('l', "use a long listing format");
                 //$this->cli->addFlag("long", array("alias" => "-l", "default" => false, "help" => "Use a long listing format"));
                 # code...
                 break;
-            
+            case 'touch':
+                /**
+                 * Usage: touch [-c] [-d DATE] [-t DATE] [-r FILE] FILE...
+                 *
+                 *   Update the last-modified date on the given FILE[s]
+                 *   
+                 *           -c      Don't create files
+                 *           -h      Don't follow links
+                 *           -d DT   Date/time to use
+                 *           -t DT   Date/time to use
+                 *           -r FILE Use FILE's date/time
+                 */
+                $this->init_cli();
+                $this->params = true;
+                $this->cli->addRule('l', "use a long listing format");
+                break;
+            case 'mkdir':
+                /**
+                 * Usage: mkdir [OPTIONS] DIRECTORY...
+                 *  Create DIRECTORY
+                 *              
+                 *          -m MODE Mode
+                 *          -p      No error if exists; make parent directories as needed
+                 */
+                $this->init_cli();
+                $this->params = true;
+                $this->cli->addRule('l', "use a long listing format");
+                break;
             default:
                 return $this->cmd_fall($cmd_name);
                 break;
